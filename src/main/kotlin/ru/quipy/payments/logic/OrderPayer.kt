2f06880c60e00dc73f81101833b065c11916fe36
Marsel Akhmetov <makhmetovr@ozon.ru>
2025-09-30 22:53:14 +0300

    add base back pressure 429

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -12,6 +12,8 @@ import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
+import java.time.Duration
+import kotlin.math.min
 
 @Service
 class OrderPayer {
@@ -27,17 +29,39 @@ class OrderPayer {
     private lateinit var paymentService: PaymentService
 
     private val paymentExecutor = ThreadPoolExecutor(
-        16,
-        16,
+        110,
+        110,
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(8_000),
+        LinkedBlockingQueue(),
         NamedThreadFactory("payment-submission-executor"),
         CallerBlockingRejectedExecutionHandler()
     )
 
+    class PaymentRejectedException(val estimatedCompletionTimestamp: Long) :
+        RuntimeException("Payment rejected due to high load. Retry after $estimatedCompletionTimestamp timestamp.") {
+        override fun fillInStackTrace(): Throwable = this
+    }
+
     fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
         val createdAt = System.currentTimeMillis()
+        val currQueueSize = paymentExecutor.queue.size
+
+//        PaymentAccountProperties(serviceName=marsel-repo, accountName=acc-18, parallelRequests=10000, rateLimitPerSec=110, price=30, averageProcessingTime=PT1S, enabled=true)
+        val parallelRequests = 10000
+        val rateLimitPerSec = 110
+        val requestAverageProcessingTime = Duration.ofSeconds(1)
+
+        val parallelSpeed = parallelRequests * 1000 / requestAverageProcessingTime.toMillis()
+        val speed = min(parallelSpeed, rateLimitPerSec.toLong())
+        val estimatedWaitTimeInSeconds = currQueueSize.toDouble() / speed
+        val estimatedWaitTime = (estimatedWaitTimeInSeconds * 1000).toLong()
+        val estimatedCompletionTime = createdAt + estimatedWaitTime
+
+        logger.warn("paymentId: {} estimatedWaitTimeInSeconds: {} sec currQueueSize: {}", paymentId, estimatedWaitTimeInSeconds, currQueueSize)
+        if (estimatedCompletionTime > deadline) {
+            throw PaymentRejectedException(estimatedCompletionTime)
+        }
         paymentExecutor.submit {
             val createdEvent = paymentESService.create {
                 it.create(
@@ -46,7 +70,7 @@ class OrderPayer {
                     amount
                 )
             }
-            logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
+            logger.warn("Payment ${createdEvent.paymentId} for order $orderId created.")
 
             paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
         }
